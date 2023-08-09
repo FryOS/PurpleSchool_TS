15eb5867815aef2bf2ee45164ad7ca136ffdd611
Ospishchev Alexey <alexeyosp@gmail.com>
2023-08-09 23:16:18 +0300

    ts объекты

---
+++ typescript/app.js
@@ -0,0 +1,18 @@
+"use strict";
+const a = 1;
+let revenue = 1000;
+let bonus = 500;
+let res = revenue + bonus;
+console.log(res);
+let info = {
+    "officeId": 45,
+    "isOpened": false,
+    "contacts": {
+        "phone": "+79100000000",
+        "email": "my@email.ru",
+        "address": {
+            "city": "Москва"
+        }
+    }
+};
+console.log(info);
