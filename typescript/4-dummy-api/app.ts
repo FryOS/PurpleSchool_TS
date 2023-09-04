be8d9490b6993101829b4e74ed9ee6377407f09f
Ospishchev Alexey <alexeyosp@gmail.com>
2023-09-04 22:12:59 +0300

    переделал объект

--- typescript/4-dummy-api/app.ts
+++ typescript/4-dummy-api/app.ts
@@ -2,19 +2,19 @@ const url = "https://dummyjson.com/users";
 
 function getUsers(url: string): Promise<User> {
   const data = fetch(url)
-    .then((res:Response) => res.json())
+    .then((res: Response) => res.json())
     .then((data) => {
       return data;
     })
     .catch((error: Error) => {
-        console.error(error);
+      console.error(error);
     });
-    return data;
+  return data;
 }
 
 const a = getUsers(url);
 
-console.log(a.then((data:User) => console.log(data)));
+console.log(a.then((data: User) => console.log(data)));
 
 interface User {
   id: number;
@@ -25,4 +25,23 @@ interface User {
   gender: string;
   email: string;
   phone: string;
+  username: string;
+  password: string;
+  birthDate: string;
+  image: string;
+  bloodGroup: string;
+  height: number;
+  weight: number;
+  eyeColor: string;
+  hair: {};
+  domain: string;
+  ip: string;
+  address: {};
+  macAddress: string;
+  university: string;
+  bank: {};
+  company: {};
+  ein: string;
+  ssn: string;
+  userAgent: string;
 }
